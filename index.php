cbd3acc4d99b2511d498f07a0fba2a05fa8f5ab9
brent <brent.gerets@gmail.com>
2023-01-10 21:25:47 +0100

    database connection test

--- index.php
+++ index.php
@@ -1,1 +1,16 @@
 <?php echo '<p>Hello World</p>'; ?>
+
+<?php
+$servername = env('DB_HOST', '127.0.0.1');
+$username = env('DB_USERNAME', 'forge');
+$password = env('DB_PASSWORD', '');
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+?>

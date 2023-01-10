ce6101a6f609f9404005fc1ffc720900e084405e
brent <brent.gerets@gmail.com>
2023-01-10 23:43:45 +0100

    database connection test

--- index.php
+++ index.php
@@ -5,14 +5,13 @@ $servername = "iotproject2023-server.mysql.database.azure.com";
 $username = "xbgvnmqlzs";
 $password = "NC37L7E8X5PJR420$";
 
-echo "<p>" . $servername . "</p>";
-
 // Create connection
 $conn = new mysqli($servername, $username, $password);
 
 // Check connection
 if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
+    echo "<p>Connection failed</p>";
+    die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully";
+echo "<p>Connected successfully</p>";
 ?>
